a2a5c0eab10706fc156d30e3ae48d252c59a24fe
K-Irungu <110017552+K-Irungu@users.noreply.github.com>
2022-11-14 14:03:48 +0300

    Set up the backend for deployment

--- cors.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'YOUR-NETLIFY_URL', 'YOUR-CUSTOM-DOMAIN'
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
-  end
