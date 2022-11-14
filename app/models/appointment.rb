d8c7a1624500a42510354c9029e6f7c43f7b57e5
first-name second-name <silvia.wachira@student.moringa.com>
2022-11-14 14:20:24 +0300

    create a class Appointment that inherits from active records

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,16 +1,16 @@
 class Appointment < ActiveRecord::Base
-    attr_accessor :specialist, :patient_name
-    belongs_to :doctor
-    belongs_to :patient
+#     attr_accessor :specialist, :patient_name
+#     belongs_to :doctor
+#     belongs_to :patient
 
-    # Here I've made the initialize method for the Appointment class responsible for making associations between the patient and doctor every time an appointment is created.
+#     # Here I've made the initialize method for the Appointment class responsible for making associations between the patient and doctor every time an appointment is created.
     
-    def initialize(specialist:, patient_name:) 
-        super
-        appointment_doctor = Doctor.find_by(doctor_name: specialist)
-        appointment_doctor.appointments << self
+#     def initialize(specialist:, patient_name:) 
+#         super
+#         appointment_doctor = Doctor.find_by(doctor_name: specialist)
+#         appointment_doctor.appointments << self
 
-        appointment_patient = Patient.find_or_create_by(patient_name: patient_name)
-        appointment_patient.appointments << self
-    end
+#         appointment_patient = Patient.find_or_create_by(patient_name: patient_name)
+#         appointment_patient.appointments << self
+#     end
 end
