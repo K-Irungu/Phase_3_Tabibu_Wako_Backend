c972bdc796210476d20f8a99c37ff691fdebe7c5
first-name second-name <silvia.wachira@student.moringa.com>
2022-11-14 14:24:20 +0300

    create a method initialize

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,12 +5,12 @@ class Appointment < ActiveRecord::Base
 
 #     # Here I've made the initialize method for the Appointment class responsible for making associations between the patient and doctor every time an appointment is created.
     
-#     def initialize(specialist:, patient_name:) 
-#         super
-#         appointment_doctor = Doctor.find_by(doctor_name: specialist)
-#         appointment_doctor.appointments << self
+    def initialize(specialist:, patient_name:) 
+        super
+        appointment_doctor = Doctor.find_by(doctor_name: specialist)
+        appointment_doctor.appointments << self
 
-#         appointment_patient = Patient.find_or_create_by(patient_name: patient_name)
-#         appointment_patient.appointments << self
-#     end
+        appointment_patient = Patient.find_or_create_by(patient_name: patient_name)
+        appointment_patient.appointments << self
+    end
 end
