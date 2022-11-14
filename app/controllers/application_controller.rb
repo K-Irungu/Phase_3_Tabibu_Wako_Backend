5ea309fbeec930d32bc9557ccacbcbceb3a9339a
K-Irungu <110017552+K-Irungu@users.noreply.github.com>
2022-11-14 09:06:16 +0300

    Completed Backend Setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,58 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+    
+  get "/doctors" do
+    doctors = Doctor.all
+    doctors.to_json(include: :appointments)
+  end
+
+  get "/doctors/:id" do
+    doctor = Doctor.find(params[:id])
+    doctor.to_json(include: :appointments)
+  end
+
+  get "/patients" do
+    patients = Patient.all
+    patients.to_json(include: :appointments)
+  end
+
+  get "/patients/:id" do
+    patient = Patient.find(params[:id])
+    patient.to_json(include: :appointments)
+  end
+
+  #Last task is to ensure your appointments include both the doctor and the patient*******
+  get "/appointments" do
+    appointments = Appointment.all
+    appointments.to_json(include: [:doctor, :patient])
+  end
+
+  get "/appointments/:id" do
+    appointment = Appointment.find(params[:id])
+    appointment.to_json(include: [:doctor, :patient])
+  end
+
+  post "/appointments" do
+    appointment = Appointment.create(specialist: params[:specialist], patient_name: params[:patient_name])
+    appointment.to_json
+  end
+
+  patch "/appointments/:id" do
+    #I'm passing in id, specialist, and patient_name
+    appointment = Appointment.find(params[:id])
+
+    updated_doctor_id = Doctor.find_by(doctor_name: params[:specialist]).id
+    updated_patient_id = Patient.find_or_create_by(patient_name: params[:patient_name]).id
+    
+    # Updating the patient and doctor ids for a specific appointment automatically updates the relationships bewteen the instances
+    appointment.update(patient_id: updated_patient_id, doctor_id: updated_doctor_id)
+    appointment.to_json
+  end
+
+  delete "/appointments/:id" do
+    appointment = Appointment.find(params[:id])
+    appointment.destroy
+    appointment.to_json
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,23 @@
 puts "🌱 Seeding spices..."
 
+d1 = Doctor.create(doctor_name: "Dr. Linda Wairimu", specialty: "Paediatrician")
+d2 = Doctor.create(doctor_name: "Dr. Kimani Kamau", specialty: "Radiologist")
+d3 = Doctor.create(doctor_name: "Dr. Olive Karmen", specialty: "Cardiologist")
+d4 = Doctor.create(doctor_name: "Dr. Matthew Hayes", specialty: "Oncologist")
+d5 = Doctor.create(doctor_name: "Dr. Kevin Irungu", specialty: "Paediatrician")
+
+p1 = Patient.create(patient_name: "Ivan Nguyo")
+p2 = Patient.create(patient_name: "Silvia Wachira")
+p3 = Patient.create(patient_name: "Ronald Kubasu")
+p4 = Patient.create(patient_name: "Ian Chege")
+p5 = Patient.create(patient_name: "Leonard Patel")
+p6 = Patient.create(patient_name: "Andrew Rono")
+p7 = Patient.create(patient_name: "Peter Munyao")
+p8 = Patient.create(patient_name: "Elton Lekashingo")
+p9 = Patient.create(patient_name: "Francis Kipchoge")
+p10 = Patient.create(patient_name: "Elijah Wandera")
+
+
 # Seed your database here
 
 puts "✅ Done seeding!"
